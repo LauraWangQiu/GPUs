c6d320aa540b7a36c69266dfb670d75c789e6434
LauraWangQiu <yiwang03@ucm.es>
2025-04-13 13:10:23 +0200

    image, nbody

--- src/lab3/nbody/nbody.c
+++ src/lab3/nbody/nbody.c
@@ -5,26 +5,31 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 
-double get_time(){
-	static struct timeval 	tv0;
+double get_time()
+{
+	static struct timeval tv0;
 	double time_, time;
 
-	gettimeofday(&tv0,(struct timezone*)0);
-	time_=(double)((tv0.tv_usec + (tv0.tv_sec)*1000000));
-	time = time_/1000000;
-	return(time);
+	gettimeofday(&tv0, (struct timezone *)0);
+	time_ = (double)((tv0.tv_usec + (tv0.tv_sec) * 1000000));
+	time = time_ / 1000000;
+	return (time);
 }
 
+typedef struct
+{
+	float m, x, y, z, vx, vy, vz;
+} body;
 
-typedef struct { float m, x, y, z, vx, vy, vz; } body;
+void randomizeBodies(body *data, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		data[i].m = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
 
-void randomizeBodies(body *data, int n) {
-	for (int i = 0; i < n; i++) {
-		data[i].m  = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-
-		data[i].x  = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-		data[i].y  = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-		data[i].z  = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+		data[i].x = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+		data[i].y = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+		data[i].z = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
 
 		data[i].vx = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
 		data[i].vy = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
@@ -32,61 +37,74 @@ void randomizeBodies(body *data, int n) {
 	}
 }
 
-void bodyForce(body *p, float dt, int n) {
+void bodyForce(body *p, float dt, int n)
+{
+	#pragma acc parallel loop collapse(2) copyin(p[0 : n]) copy(p[0 : n])
+	for (int i = 0; i < n; i++)
+	{
+		float Fx = 0.0f;
+		float Fy = 0.0f;
+		float Fz = 0.0f;
 
-	for (int i = 0; i < n; i++) { 
-		float Fx = 0.0f; float Fy = 0.0f; float Fz = 0.0f;
-
-		for (int j = 0; j < n; j++) {
-			if (i!=j) {
+		for (int j = 0; j < n; j++)
+		{
+			if (i != j)
+			{
 				float dx = p[j].x - p[i].x;
 				float dy = p[j].y - p[i].y;
 				float dz = p[j].z - p[i].z;
-				float distSqr = dx*dx + dy*dy + dz*dz;
+				float distSqr = dx * dx + dy * dy + dz * dz;
 				float invDist = 1.0f / sqrtf(distSqr);
 				float invDist3 = invDist * invDist * invDist;
 
 				float G = 6.674e-11;
 				float g_masses = G * p[j].m * p[i].m;
 
-				Fx += g_masses * dx * invDist3; 
-				Fy += g_masses * dy * invDist3; 
+				Fx += g_masses * dx * invDist3;
+				Fy += g_masses * dy * invDist3;
 				Fz += g_masses * dz * invDist3;
 			}
 		}
 
-		p[i].vx += dt*Fx/p[i].m; p[i].vy += dt*Fy/p[i].m; p[i].vz += dt*Fz/p[i].m;
+		p[i].vx += dt * Fx / p[i].m;
+		p[i].vy += dt * Fy / p[i].m;
+		p[i].vz += dt * Fz / p[i].m;
 	}
 }
 
-void integrate(body *p, float dt, int n){
-	for (int i = 0 ; i < n; i++) {
-		p[i].x += p[i].vx*dt;
-		p[i].y += p[i].vy*dt;
-		p[i].z += p[i].vz*dt;
+void integrate(body *p, float dt, int n)
+{
+	#pragma acc parallel loop copy(p[0:n])
+	for (int i = 0; i < n; i++)
+	{
+		p[i].x += p[i].vx * dt;
+		p[i].y += p[i].vy * dt;
+		p[i].z += p[i].vz * dt;
 	}
 }
 
-int main(const int argc, const char** argv) {
-
+int main(const int argc, const char **argv)
+{
 	int nBodies = 1000;
-	if (argc > 1) nBodies = atoi(argv[1]);
+	if (argc > 1)
+		nBodies = atoi(argv[1]);
 
 	const float dt = 0.01f; // time step
-	const int nIters = 100;  // simulation iterations
+	const int nIters = 100; // simulation iterations
 
-	body *p = (body*)malloc(nBodies*sizeof(body));
+	body *p = (body *)malloc(nBodies * sizeof(body));
 
 	randomizeBodies(p, nBodies); // Init pos / vel data
 
 	double t0 = get_time();
 
-	for (int iter = 1; iter <= nIters; iter++) {
+	for (int iter = 1; iter <= nIters; iter++)
+	{
 		bodyForce(p, dt, nBodies); // compute interbody forces
 		integrate(p, dt, nBodies); // integrate position
 	}
 
-	double totalTime = get_time()-t0; 
+	double totalTime = get_time() - t0;
 	printf("%d Bodies with %d iterations: %0.3f Millions Interactions/second\n", nBodies, nIters, 1e-6 * nBodies * nBodies / totalTime);
 
 	free(p);
